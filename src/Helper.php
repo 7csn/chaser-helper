aef06d5c2468833366c6d5b5d6d8f59f1ea09ea1
7csn <1479672753@qq.com>
2020-08-24 15:07:11 +0800

    add 身份证、银行卡校验，文本截取

--- src/Helper.php
+++ src/Helper.php
@@ -72,4 +72,69 @@ class Helper
         $float = substr($end, 0, 8) - substr($start, 0, 8);
         return round($int + $float, 6);
     }
+
+    /**
+     * 银行卡校验
+     *
+     * @param string $number
+     * @return bool
+     */
+    public static function bankCardCheck(string $number)
+    {
+        $length = strlen($number);
+        $oddSum = $evenSum = 0;
+        $isOdd = true;
+        for ($i = $length - 1; $i >= 0; $i--) {
+            if ($isOdd) {
+                $oddSum += $number[$i];
+            } else {
+                $even = $number[$i] * 2;
+                $even > 9 && $even -= 9;
+                $evenSum += $even;
+            }
+            $isOdd = !$isOdd;
+        }
+        return ($oddSum + $evenSum) % 10 === 0;
+    }
+
+    /**
+     * 身份证校验
+     *
+     * @param string $number
+     * @return bool
+     */
+    public static function idCardCheck(string $number)
+    {
+        if (strlen($number) !== 18) {
+            return false;
+        }
+
+        $weight = 0;
+
+        // 权值
+        $W = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+
+        // 校验码
+        $Y = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'];
+
+        for ($i = 0; $i < 17; $i++) {
+            $weight += $number[$i] * $W[$i];
+        }
+
+        return $Y[$weight % 11] === substr($number, -1);
+    }
+
+    /**
+     * 截取文本
+     *
+     * @param string $text
+     * @param int $length
+     * @param string $charset
+     * @return string
+     */
+    public static function cutText(string $text, int $length, $charset = 'utf-8')
+    {
+        $mbLength = mb_strlen($text, $charset);
+        return $mbLength > $length ? mb_substr($text, 0, max($length - 2, 0), $charset) . '...' : $text;
+    }
 }
